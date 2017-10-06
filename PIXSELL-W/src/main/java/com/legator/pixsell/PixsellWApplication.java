d1bd23c9982dc54d38a2ebf244d5a13872400fb5
tharindu99 <madushanka.best@gmail.com>
2017-10-06 14:43:17 +0530

    spring boot project initialization started

---
+++ PIXSELL-W/src/main/java/com/legator/pixsell/PixsellWApplication.java
@@ -0,0 +1,12 @@
+package com.legator.pixsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixsellWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixsellWApplication.class, args);
+	}
+}
